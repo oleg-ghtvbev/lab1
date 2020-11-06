c4d2b95069f163b1c89322a8cef9bdd67df65a3e
oleg-ghtvbev <o.fiodorov2014@yandex.ru>
2020-11-06 21:03:52 +0400

    work_to_skript

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let names = 'Petr';
-var name2 = 'Artur';
-const name3 = 'dad';
-let  hello = `hello ${names}`;
-let  hello2 = `hello ${name2}`;
-let  hello3 = `hello ${name3}`;
-console.log(hello);
-console.log(hello2);
-console.log(hello3);
+// let names = 'Petr';
+// var name2 = 'Artur';
+// const name3 = 'dad';
+// let privet = 'привет'
+// let  hello = `${privet} ${names}`;
+// let  hello2 = `${privet} ${name2}`;
+// let  hello3 = `${privet} ${name3}`;
+// console.log(hello);
+// console.log(hello2);
+// console.log(hello3);
+ let privet = ['Привет', 'hello']
+let array = ['petr', 'frtur', 'dad']
+for (let index = 0; index < array.length; index++){
+    for (let priv = 0; priv < privet.length; priv++){
+    console.log(array[index] +' '+privet[priv])}
+}
