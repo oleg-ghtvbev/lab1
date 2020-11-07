32bb9c9c1e4656f5bb6dd218e0aaced1826b40d7
oleg-ghtvbev <o.fiodorov2014@yandex.ru>
2020-11-07 21:38:55 +0400

    scripton

--- script.js
+++ script.js
@@ -8,9 +8,58 @@
 // console.log(hello);
 // console.log(hello2);
 // console.log(hello3);
- let privet = ['Привет', 'hello']
-let array = ['petr', 'frtur', 'dad']
-for (let index = 0; index < array.length; index++){
-    for (let priv = 0; priv < privet.length; priv++){
-    console.log(array[index] +' '+privet[priv])}
+//  let privet = ['Привет', 'hello']
+// let array = ['petr', 'frtur', 'dad']
+// for (let index = 0; index < array.length; index++){
+//     for (let priv = 0; priv < privet.length; priv++){
+//     console.log(`${array[index]}     ${privet[priv]}`)
+// }
+// }
+
+// let model = ['audi', 'bmv', 'leksus']
+// let avto = ['bas', 'mobil']
+// for(let dvig = 0; dvig < model.length; dvig++){
+//     for(let dving = 0; dving < avto.length; dving++){
+//         console.log(`${model[dvig]} ${avto[dving]}`)
+//     }
+// }
+
+let name = {
+    key1: "Aton",
+    key2: "Vent",
+    key3: "Bro",
 }
+
+let fameli = {
+    fam1: "gotr",
+    fam2: "gray",
+    fam3: "gjhk",
+} 
+
+const danc = {
+    dans1: "pol",
+    dans2: "fdfl",
+    dans3: "ksksfj",
+}
+// for (let form in name){
+//     console.log(`${form}:${name[form]}`) 
+// }
+
+//  for (let key in fameli){
+//      console.log(`${key}:${fameli[key]}`)
+//  }
+
+//  for (let names in danc){
+//      console.log(`${names}:${danc[names]}`)
+//  }
+ console.log(name.key1)
+ console.log(name.key2)
+ console.log(name.key3)
+ console.log(fameli.fam1)
+ console.log(name.fem2)
+
+ for (let name in danc){
+     console.log(`${name}:${danc[name]}`)
+ }
+
+
